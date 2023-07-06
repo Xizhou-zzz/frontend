f65f7a6bea7a798ff614df8c1a4575fe10921005
tjw <1485891941@qq.com>
2023-07-06 14:30:42 +0800

    Update User.vue

--- src/pages/User.vue
+++ src/pages/User.vue
@@ -7,5 +7,45 @@
 
 
 <template>
-    
+  <div class="absolute inset-0 bg-tjw-sb -z-50"></div>
+
+  <div class="absolute inset-0">
+    <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex items-center h-16">
+        <div class="flex-shrink-0">
+          <h1 class="text-white text-lg font-semibold">My Website</h1>
+        </div>
+        <div class="hidden md:block ml-10">
+          <div class="flex items-baseline space-x-4">
+            <a
+              href="#"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Services
+            </a>
+            <a
+              href="#"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+  
+  
 </template>
