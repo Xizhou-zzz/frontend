3e4c35d03b9470b53b34ca2e9213bff6c7360936
tjw <1485891941@qq.com>
2023-07-07 11:15:18 +0800

    整合界面

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,9 +1,4 @@
 <script lang="ts">
-<<<<<<< HEAD
-
-export default {
-  name: "LoginPage",
-=======
 import axios from "axios";
  export default {
   name:"Register",
@@ -14,7 +9,6 @@ import axios from "axios";
       againpassword: ''
     };
   },
->>>>>>> c1d86d5df5f29a2eceae91ab6be1f5c97e0f74f5
   methods: {
     gotoLogin() {
       this.$router.push('/')
@@ -51,20 +45,6 @@ import axios from "axios";
     <div class="m-2 flex flex-col gap-2 basis-1/2">
 
       <div class="text-center basis-1/3">
-<<<<<<< HEAD
-        <Label for="username" class="mb-2">账号</Label>
-        <Input class=" rounded" type="text" placeholder="请输入账号..." />
-      </div>
-
-      <div class="text-center basis-1/3">
-        <Label for="password" class="mb-2">密码</Label>
-        <Input rounded class=" rounded" type="password" placeholder="请输入密码..." />
-      </div>
-
-      <div class="text-center basis-1/3 mr-20">
-        <Label for="password" class="mb-2">请再次输入密码</Label>
-        <Input rounded class=" rounded" type="password" placeholder="请再次输入密码..." />
-=======
         <label for="username" class="mb-2">账号</label>
         <input
           class = " rounded" 
@@ -94,19 +74,13 @@ import axios from "axios";
           placeholder="请再次输入密码..."
           v-model="againpassword"
         />
->>>>>>> c1d86d5df5f29a2eceae91ab6be1f5c97e0f74f5
       </div>
 
     </div>
 
     <div class="flex gap-4 justify-center basis-1/4">
-<<<<<<< HEAD
-      <button @click="gotoLoginpage" class="rounded outline outline-offset-2 outline-cyan-500 ... w-1/4 h-1/2">登录</button>
-      <button class="rounded outline outline-offset-2 outline-cyan-500 ... w-1/4 h-1/2" color="alternative">注册</button>
-=======
       <button  @click="gotoLogin" class="rounded bg-indigo-500 ... ... w-1/4 h-1/2">登录</button>
       <button class= "rounded outline outline-offset-2 outline-cyan-500 ... w-1/4 h-1/2" color="alternative" >注册</button>
->>>>>>> c1d86d5df5f29a2eceae91ab6be1f5c97e0f74f5
     </div>
   </Card>
 </template>
