23c8971c88ecfbe0910e9b3bdb48d4409c7f0631
TLectronic <3051732820@qq.com>
2023-07-08 19:27:30 +0800

    注册界面约束条件添加

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -18,6 +18,12 @@ export default {
     goRegister() {
       if (this.password != this.againpassword)
         this.message = '前后两次输入密码不同'
+      else if (this.username == "") {
+        this.message = '用户名不能为空'
+      }
+      else if (this.password == "") {
+        this.message = '密码不能为空'
+      }
       else {
         this.message = ''
         axios.post('http://localhost:5000/api/Register',
@@ -48,22 +54,22 @@ export default {
 
     <div class="m-2 flex flex-col gap-2 basis-1/2">
 
-      <div class="text-center basis-1/3">
+      <div class="text-center basis-1/4">
         <label for="username" class="mb-2 ml-0 font-semibold">用户名</label>
         <input class=" rounded" type="text" placeholder="请输入用户名..." v-model="username" />
       </div>
 
-      <div class="text-center basis-1/3">
+      <div class="text-center basis-1/4">
         <label for="password" class="mb-2 ml-3 font-semibold">密码</label>
         <input rounded class=" rounded" type="password" placeholder="请输入密码..." v-model="password" />
       </div>
 
-      <div class="text-center basis-1/3 mr-20">
+      <div class="text-center basis-1/4 mr-20">
         <label for="password" class="mb-2 ml-3 font-semibold">请再次输入密码</label>
         <input rounded class=" rounded" type="password" placeholder="请再次输入密码..." v-model="againpassword" />
       </div>
 
-      <span class="text-red-400 text-center">{{ message }}</span>
+      <div class="text-red-400 text-center basis-1/4">{{ message }}</div>
 
     </div>
 
