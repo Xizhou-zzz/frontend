9fdfad257b6f42f46a4252c548bbc619f72b9611
Xizhou-zzz <1158398445@qq.com>
2023-07-09 19:02:23 +0800

    beautify the shit

--- src/pages/Visualtwo.vue
+++ src/pages/Visualtwo.vue
@@ -1,73 +1,45 @@
-<script>
-import * as echarts from 'echarts';
-import axios from "axios";
-export default {
-    name: "Visualtwo",
+<script setup>
+    import * as echarts from 'echarts';
+    import axios from "axios";
+    import { getVisualTwoData } from '../lib/axios'
 
-    mounted() {
-        this.chart = echarts.init(this.$refs.chart);
-        this.renderChart();
-    },
-    methods: {
-        gotoLogin() {
-            this.$router.push('/')
-        },
-        getDataFromServer1() {
-            this.$router.push('/Visualone')
-            // axios.get('http://localhost:5000/api/Visualone')
-            //     .then(response => {
-            //         const data = response.data;  // 获取后端发送的数据
-            //         this.renderChart(data);  // 将数据传递给renderChart方法
-            //     })
-            //     .catch(error => {
-            //         console.error(error);
-            //     });
-        },
-        getDataFromServer2() {
-            // this.$router.push('/Visualtwo')
-            axios.get('http://localhost:5000/api/Visualtwo')
-                .then(response => {
-                    const data = response.data;  // 获取后端发送的数据
-                    this.renderChart(data);  // 将数据传递给renderChart方法
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        },
-        getDataFromServer3() {
-            this.$router.push('/Visualthree')
-            // axios.get('http://localhost:5000/api/Visualthree')
-            //     .then(response => {
-            //         const data = response.data;  // 获取后端发送的数据
-            //         this.renderChart(data);  // 将数据传递给renderChart方法
-            //     })
-            //     .catch(error => {
-            //         console.error(error);
-            //     });
-        },
-        renderChart(data) {
-            const options = {
-                xAxis: {
-                    data: ['0:00-0:59', '1:00-1:59', '2:00-2:59', '3:00-3:59',
-                        '4:00-4:59', '5:00-5:59', '6:00-6:59', '7:00-7:59',
-                        '8:00-8:59', '9:00-9:59', '10:00-10:59', '11:00-11:59',
-                        '12:00-12:59', '13:00-13:59', '14:00-14:59', '15:00-15:59',
-                        '16:00-16:59', '17:00-17:59', '18:00-18:59', '19:00-19:59',
-                        '20:00-20:59', '21:00-21:59', '22:00-22:59', '23:00-23:59']
-                },
-                yAxis: {},
-                series: [
-                    {
-                        type: 'line',
-                        data: data
-                    }
-                ]
-            };
-            this.chart.setOption(options);
-        },
+    import { useRouter } from 'vue-router'
+    import { onMounted, ref } from 'vue';
+    const router = useRouter()
 
+
+    let echart;
+    const chart = ref(null)
+    onMounted(async () => {
+        echart = echarts.init(chart.value)
+        updateChart();
+    })
+
+    async function updateChart() {
+        const data = await getVisualTwoData()
+        renderChart(data);
     }
-}
+
+    function renderChart(data) {
+        const options = {
+            xAxis: {
+                data: ['0:00-0:59', '1:00-1:59', '2:00-2:59', '3:00-3:59',
+                    '4:00-4:59', '5:00-5:59', '6:00-6:59', '7:00-7:59',
+                    '8:00-8:59', '9:00-9:59', '10:00-10:59', '11:00-11:59',
+                    '12:00-12:59', '13:00-13:59', '14:00-14:59', '15:00-15:59',
+                    '16:00-16:59', '17:00-17:59', '18:00-18:59', '19:00-19:59',
+                    '20:00-20:59', '21:00-21:59', '22:00-22:59', '23:00-23:59']
+            },
+            yAxis: {},
+            series: [
+                {
+                    type: 'line',
+                    data: data
+                }
+            ]
+        };
+        echart.setOption(options);
+    }
 </script>
 
 <template>
@@ -104,7 +76,8 @@ export default {
                 </div>
 
                 <div class="absolute top-3 right-5">
-                    <button @click="gotoLogin" class="bg-exit bg-cover bg-center py-4 px-4 rounded" title="退出登录">
+                    <button @click="() => {router.push('/')}" class="bg-exit bg-cover bg-center py-4 px-4 rounded"
+                        title="退出登录">
                     </button>
                 </div>
 
@@ -123,16 +96,18 @@ export default {
                         <div class="absolute left-1 ml-2 text-white rounded-md text-sm font-bold">
                             请选择图表类型:
                         </div>
-                        <button class="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-bold"
-                            @click="getDataFromServer1">
+                        <button
+                            class="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-bold"
+                            @click="() => {router.push('/Visualone')}">
                             订单数按星期分布饼状图
                         </button>
                         <button class="text-gray-300 px-3 py-2 rounded-md text-sm font-bold bg-gray-700"
-                            @click="getDataFromServer2">
+                            @click="updateChart">
                             不同时间单车需求散点图
                         </button>
-                        <button class="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-bold"
-                            @click="getDataFromServer3">
+                        <button
+                            class="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-bold"
+                            @click="() => {router.push('/Visualthree')}">
                             不同位置单车需求热力图
                         </button>
                     </div>
