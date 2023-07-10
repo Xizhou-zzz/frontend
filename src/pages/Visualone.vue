99a4b05b74b8eefd4f7a01a68a6612da7954260f
TLectronic <3051732820@qq.com>
2023-07-10 09:10:24 +0800

    Update Visualone.vue

--- src/pages/Visualone.vue
+++ src/pages/Visualone.vue
@@ -1,86 +1,89 @@
 <script setup>
-  import * as echarts from 'echarts';
-  import axios from "axios";
+import * as echarts from 'echarts';
+import axios from "axios";
 
-  import { onMounted, ref } from 'vue';
-  import { useRouter } from 'vue-router'
-  import { getVisualOneData } from '../lib/axios'
-  import { getVisualTwoData } from '../lib/axios'
-  import { getVisualThreeData } from '../lib/axios'
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router'
+import { getVisualOneData } from '../lib/axios'
+import { getVisualTwoData } from '../lib/axios'
+import { getVisualThreeData } from '../lib/axios'
 
-  const tab = ref('one')
+const tab = ref('one')
 
-  const router = useRouter()
+const router = useRouter()
 
-  let echart
-  const chart = ref(null)
+let echart
+const chart = ref(null)
 
-  onMounted(() => {
-    echart = echarts.init(chart.value);
-    renderChart();
-  })
+onMounted(() => {
+  echart = echarts.init(chart.value);
+  renderChart();
+})
 
-  async function renderChart() {
-    echart.dispose();
-    echart = echarts.init(chart.value);
-    const data = await getVisualOneData()
-    console.log(data)
-    const options = {
-      series: [
-        {
-          type: 'pie',
-          data: data
-        }
-      ]
-    };
+async function renderChart() {
+  echart.dispose();
+  echart = echarts.init(chart.value);
+  echart.showLoading();//显示加载动画
+  const data = await getVisualOneData()
+  // console.log(data)
+  const options = {
+    series: [
+      {
+        type: 'pie',
+        data: data
+      }
+    ]
+  };
+  echart.hideLoading();//隐藏加载动画
+  echart.setOption(options);//渲染图表
+}
 
-    echart.setOption(options);
-  }
+async function renderChart2() {
+  echart.dispose();
+  echart = echarts.init(chart.value);
+  echart.showLoading();//显示加载动画
+  const data = await getVisualTwoData()
+  const options = {
+    xAxis: {
+      data: ['0:00-0:59', '1:00-1:59', '2:00-2:59', '3:00-3:59',
+        '4:00-4:59', '5:00-5:59', '6:00-6:59', '7:00-7:59',
+        '8:00-8:59', '9:00-9:59', '10:00-10:59', '11:00-11:59',
+        '12:00-12:59', '13:00-13:59', '14:00-14:59', '15:00-15:59',
+        '16:00-16:59', '17:00-17:59', '18:00-18:59', '19:00-19:59',
+        '20:00-20:59', '21:00-21:59', '22:00-22:59', '23:00-23:59']
+    },
+    yAxis: {},
+    series: [
+      {
+        type: 'line',
+        data: data
+      }
+    ]
+  };
+  echart.hideLoading();//隐藏加载动画
+  echart.setOption(options);//渲染图表
+}
 
-  async function renderChart2() {
-    const data = await getVisualTwoData()
-    echart.dispose();
-    echart = echarts.init(chart.value);
-    const options = {
-      xAxis: {
-        data: ['0:00-0:59', '1:00-1:59', '2:00-2:59', '3:00-3:59',
-          '4:00-4:59', '5:00-5:59', '6:00-6:59', '7:00-7:59',
-          '8:00-8:59', '9:00-9:59', '10:00-10:59', '11:00-11:59',
-          '12:00-12:59', '13:00-13:59', '14:00-14:59', '15:00-15:59',
-          '16:00-16:59', '17:00-17:59', '18:00-18:59', '19:00-19:59',
-          '20:00-20:59', '21:00-21:59', '22:00-22:59', '23:00-23:59']
-      },
-      yAxis: {},
-      series: [
-        {
-          type: 'line',
-          data: data
-        }
-      ]
-    };
-    echart.setOption(options);
-  }
-
-  async function renderChart3() {
-    echart.dispose();
-    echart = echarts.init(chart.value);
-    var data = [
-      [0, 0, 10],
-      [1, 0, 15],
-      [2, 0, 20],
-    ];
-    const options = {
-      xAxis: {},
-      yAxis: {},
-      series: [
-        {
-          type: 'heatmap',
-          data: data
-        }
-      ]
-    };
-    echart.setOption(options);
-  }
+async function renderChart3() {
+  echart.dispose();
+  echart = echarts.init(chart.value);
+  var data = [
+    [0, 0, 10],
+    [1, 0, 15],
+    [2, 0, 20],
+  ];
+  const options = {
+    xAxis: {},
+    yAxis: {},
+    series: [
+      {
+        type: 'heatmap',
+        data: data
+      }
+    ]
+  };
+  echart.setOption(options);
+}
 
 
 </script>
