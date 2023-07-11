c3f2b3d3eb132ae6a711fd1f8d569ed9f739f8ae
Xizhou-zzz <1158398445@qq.com>
2023-07-11 23:31:04 +0800

    修改用户信息完成

--- src/pages/Manageuser.vue
+++ src/pages/Manageuser.vue
@@ -18,21 +18,21 @@ let people = ref([])//表格数据
 let dataLoaded = ref(false)//数据是否加载完成
 let showEmptyRow = ref(false)// 控制是否显示空行
 let editable = ref([])//控制表格某行是否可编辑
-let emptyRow =  { name: '', password: '' ,type:''}// 空行数据对象
+let emptyRow = { name: '', password: '', type: '' }// 空行数据对象
+let editingRow = ref(-1)
 
 
-
-function addOne(){
+function addOne() {
     showEmptyRow.value = true
 }
 
-function updateOne(index){
+function updateOne(index) {
     editable[index] = !editable[index]
 }
 
 //点击取消后取消增加一行
-function canceladd(){
-    emptyRow = { name: '', password: '' ,type:''}
+function canceladd() {
+    emptyRow = { name: '', password: '', type: '' }
     showEmptyRow.value = false
 }
 
@@ -47,15 +47,20 @@ async function refreshdeleteddata(person) {
 }
 //刷新增加一行后的数据
 async function refreshaddeddata() {
-    await addRow({name:emptyRow.name,password:emptyRow.password,type:emptyRow.type})
+    await addRow({ name: emptyRow.name, password: emptyRow.password, type: emptyRow.type })
     await getUserData()
-    emptyRow = { name: '', password: '' ,type:''}
+    emptyRow = { name: '', password: '', type: '' }
     showEmptyRow.value = false
 }
 //刷新更改一行后的数据
-async function refreshupdateddata(){
+async function refreshupdateddata() {
 
 }
+
+const editUserName = ref('')
+const editPassword = ref('')
+const editType = ref(0)
+
 
 </script>
 
@@ -88,7 +93,8 @@ async function refreshupdateddata(){
         </el-button>
         <!-- 表格 -->
         <div class="flex justify-center table-container max-h-full overflow-y-auto space-x-10">
-            <table v-if="dataLoaded" class="table bg-white h-full w-5/6 my-4 border-collapse border border-slate-400 table-fixed">
+            <table v-if="dataLoaded"
+                class="table bg-white h-full w-5/6 my-4 border-collapse border border-slate-400 table-fixed">
                 <thead>
                     <tr>
                         <th class="px-4 py-2 border border-slate-300">用户名</th>
@@ -99,44 +105,62 @@ async function refreshupdateddata(){
                 </thead>
                 <tbody>
                     <tr v-for="(person, index) in people" :key="index">
-                        <!-- <td class="px-4 py-2 text-center border border-slate-300">
-                            <input v-if="editable[index]" v-model="person.username" />
-                            <span v-else>{{ person.username }}</span>
-                        </td> -->
 
-                        <td class="px-4 py-2 text-center border border-slate-300" v-if="editable[index]">
-                            <input v-model="person.username" />
+                        <td class="px-4 py-2 text-center border border-slate-300" v-if="editingRow == index">
+                            <input v-model="person.username" class="px-4 py-2 border border-slate-300 mx-auto" />
                         </td>
                         <td class="px-4 py-2 text-center border border-slate-300" v-else>
                             {{ person.username }}
                         </td>
 
-                        <td class="px-4 py-2 text-center border border-slate-300">
-                            <input v-if="editable[index]" v-model="person.password" />
-                            <span v-else class="disabled">{{ person.password }}</span>
+                        <td class="px-4 py-2 text-center border border-slate-300" v-if="editingRow == index">
+                            <input v-model="person.password" class="px-4 py-2 border border-slate-300 mx-auto" />
                         </td>
-                        <td class="px-4 py-2 text-center border border-slate-300">
-                            <input v-if="editable[index]" v-model="person.typology" />
-                            <span v-else>{{ person.typology }}</span>
+                        <td class="px-4 py-2 text-center border border-slate-300" v-else>
+                            {{ person.password }}
                         </td>
+
+                        <td class="px-4 py-2 text-center border border-slate-300" v-if="editingRow == index">
+                            <input v-model="person.typology" class="px-4 py-2 border border-slate-300 mx-auto" />
+                        </td>
+                        <td class="px-4 py-2 text-center border border-slate-300" v-else>
+                            {{ person.typology }}
+                        </td>
+
                         <td class="px-4 py-2 text-center border border-slate-300 space-x-1">
-                            <el-button type="primary" round  @click="refreshdeleteddata(person)">删除</el-button>
-                            <el-button type="primary" round @click="updateOne(index)">修改</el-button>
+                            <el-button type="primary" round @click="refreshdeleteddata(person)">删除</el-button>
+                            <el-button type="primary" round @click="() => {
+                                editUserName = person.username
+                                editPassword = person.password
+                                editType = person.typology
+                                editingRow = index
+                            }" v-if="editingRow != index">修改</el-button>
+                            <el-button type="primary" round @click="() => {
+                                editingRow = -1
+
+                                
+                            }" v-else>确定</el-button>
                         </td>
                     </tr>
                     <tr v-if="showEmptyRow">
                         <!-- 新增加的一行 -->
-                        <td class="text-center border border-slate-300"><input class="px-4 py-2 border border-slate-300 mx-auto" v-model="emptyRow.name" placeholder="Enter name"></td>
-                        <td class="text-center border border-slate-300"><input class="px-4 py-2 border border-slate-300 mx-auto" v-model="emptyRow.password" placeholder="Enter password"></td>
-                        <td class="text-center border border-slate-300"><input class="px-4 py-2 border border-slate-300 mx-auto" v-model="emptyRow.type" placeholder="Enter type"></td>
+                        <td class="text-center border border-slate-300"><input
+                                class="px-4 py-2 border border-slate-300 mx-auto" v-model="emptyRow.name"
+                                placeholder="Enter name"></td>
+                        <td class="text-center border border-slate-300"><input
+                                class="px-4 py-2 border border-slate-300 mx-auto" v-model="emptyRow.password"
+                                placeholder="Enter password"></td>
+                        <td class="text-center border border-slate-300"><input
+                                class="px-4 py-2 border border-slate-300 mx-auto" v-model="emptyRow.type"
+                                placeholder="Enter type"></td>
                         <!-- 确认增加或取消增加 -->
                         <td class="px-4 py-2 space-x-1 text-center border border-slate-300">
                             <el-button type="primary" round @click="refreshaddeddata">
                                 确认
-                             </el-button>
-                             <el-button type="primary" round @click="canceladd">
+                            </el-button>
+                            <el-button type="primary" round @click="canceladd">
                                 取消
-                             </el-button>
+                            </el-button>
                         </td>
                     </tr>
 
@@ -156,5 +180,4 @@ async function refreshupdateddata(){
 th,
 td {
     @apply px-4 py-2;
-}
-</style>
+}</style>
